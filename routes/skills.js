a5ba05b350e709eefde82dc756ed866a9ed8b7f3
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-07 19:09:10 -0400

    Fix bug

--- routes/skills.js
+++ routes/skills.js
@@ -4,11 +4,8 @@ import * as skillsCtrl from '../controllers/skills.js'
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('skills/index', {
-    skills: skills
-  })
-})
+
+router.get('/', skillsCtrl.index)
 
 export {
   router
